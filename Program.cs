8aa2107bba735348c45d2e09c7cfcc3336d3b975
SourceNZ <big.willy.99@hotmail.com>
2016-10-07 21:20:24 +1300

    add has full functionality now. need to change the values added to the table to be upper case

--- Program.cs
+++ Program.cs
@@ -11,9 +11,10 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
+            NotFound:
             string line;
             Console.Write(">:");
-            while (!((line = Console.ReadLine().ToLower()) == "exit"))
+            while (!((line = Console.ReadLine()).ToLower() == "exit"))
             {
 
                 using (AMBULANCES_STAFF_CONTEXT db = new AMBULANCES_STAFF_CONTEXT())
@@ -21,17 +22,133 @@ namespace ConsoleApplication1
                     string[] a = line.ToLower().Split(new String[] {" as "}, StringSplitOptions.None);
                     string[] b = line.ToLower().Split(new String[] { " id " }, StringSplitOptions.None);
                     string[] c = line.Split(' ');
+                    string test;
                     if (c[0].Equals("add"))
                     {
-                        //c = line.Split(new String[] { "add " }, StringSplitOptions.None);
-                        //Console.WriteLine(c + "WILL");
-                        if(line.IndexOf(" id ") == -1)
+                        var new_staff = new AMBULANCE_STAFF();
+                        //if there is no id then print an error message
+                        if (line.IndexOf(" id ") == -1)
                         {
                             Console.WriteLine("An officer must have a six digit ID number");
+                            goto NotFound;
                         }
+                        //if there is no as then print error message
                         if (line.IndexOf(" as ") == -1)
                         {
                             Console.WriteLine("An officer musthave a skill level(basic, intermediate, or advanced)");
+                            goto NotFound;
+                        }
+                        //if there is an id then get the index of that and substring it with just after add and those are all the specified names, if there are less than 2 names print and error
+                        // and if there are more than 2 names make all the names but the last one first names then the last one a last name
+                        if (!(line.IndexOf(" id ") == -1))
+                        {
+                            
+                            test = line.Substring(4, line.IndexOf(" id ")-4);
+
+                            string[] testarray = test.Split(' ');
+                            for(int i = 0; i <testarray.Length; i++)
+                            {
+                                Console.WriteLine(testarray[i] + " HERE BOY");
+
+                            }
+                            if(testarray.Length < 2)
+                            {
+                                Console.WriteLine("An officer must have a surname and at least one given name");
+                                goto NotFound;
+                            }
+                            else if(testarray.Length == 2)
+                            {
+                                //check if the user has entered a number for either the first name or last name
+                                int num;
+                                bool isNum = Int32.TryParse(testarray[0], out num);
+                                if (isNum)
+                                {
+                                    Console.WriteLine("An officer must have a surname and at least one given name");
+                                    goto NotFound;
+                                }
+                                else
+                                {
+                                    new_staff.FirstName = testarray[0];  
+                                }
+                                int num1;
+                                bool isNum1 = Int32.TryParse(testarray[0], out num1);
+                                if (isNum1)
+                                {
+                                    Console.WriteLine("An officer must have a surname and at least one given name");
+                                    goto NotFound;
+                                }
+                                else
+                                {
+                                    new_staff.Surname = testarray[1]; 
+                                }
+
+
+
+                            }
+                            else if(testarray.Length > 2){
+                                string newname = "";
+                                for (int i = 0; i < (testarray.Length - 1); i++)
+                                {
+                                    int num;
+                                    bool isNum = Int32.TryParse(testarray[i], out num);
+                                    if (isNum)
+                                    {
+                                        Console.WriteLine("An officer must have a surname and at least one given name");
+                                        goto NotFound;
+                                    }
+                                    else
+                                    {
+                                        newname = newname + " " + testarray[i];
+                                    }
+                                }
+                                new_staff.FirstName = newname; 
+                                int num2;
+                                bool isNum2 = Int32.TryParse(testarray[testarray.Length - 1], out num2);
+                                if (isNum2)
+                                {
+                                    Console.WriteLine("An officer must have a surname and at least one given name");
+                                    goto NotFound;
+                                }
+                                else
+                                {
+                                    new_staff.Surname = testarray[testarray.Length - 1];  
+                                }
+                           
+                                //Console.WriteLine(newname + " last name: {0}  THIS IS THE NEW NAME FOR LOTS OF NAMES", testarray[testarray.Length - 1]);
+                            }
+                            //if there is an id and and as, then get the number between those two words and it should be the ID # then verify its a 6 digit number DONE
+                            if (!(line.IndexOf(" id ") == -1))
+                            {
+                                if (!(line.IndexOf(" as ") == -1))
+                                {
+                                    test = line.Substring(line.IndexOf(" id ") , line.Length- (line.IndexOf(" as ") -1));
+                                    test = test.Replace(" id ", "");
+                                    test = test.Replace(" ", "");
+                                    test = test.Replace("as", "");
+                                    int num;
+                                    bool isNum = Int32.TryParse(test, out num);
+
+                                    if (isNum)
+                                    {
+                                        
+                                        if (test.Length == 6)
+                                        {
+                                            new_staff.OfficerID = num; 
+                                        }
+
+                                    }
+                                    else
+                                    {
+                                        Console.WriteLine("The officer ID must be a six digit number");
+                                        goto NotFound;
+                                    }
+
+                                    
+                                }
+                            }
+
+
+
                         }
 
                         string v = line.Replace("add ", "");
@@ -40,6 +157,29 @@ namespace ConsoleApplication1
                         string[] d = v.Split(' ');
                         Console.WriteLine(d.Length + "dfddsdsd");
                         //if the user enters 4 different words either correctly.
+                        string caseSwitch = d[d.Length - 1];
+                        switch (caseSwitch)
+                        {
+                            case "basic":
+                                new_staff.SkillLevel = "Basic"; 
+                                Console.WriteLine("basic BITCH");
+                                break;
+                            case "intermediate":
+                                new_staff.SkillLevel = "Intermediate"; 
+                                Console.WriteLine("intermediate BITCH");
+                                break;
+                            case "advanced":
+                                new_staff.SkillLevel = "Advanced"; 
+                                Console.WriteLine("advanced BITCH");
+                                break;
+                            default:
+                                Console.WriteLine("The skill level for the officer must be one of basic, intermediate, or advanced");
+                                goto NotFound;
+
+                        }
+                        
+                       // int offid = Convert.ToInt32(line[d.Length - 2]);
+                        //Console.WriteLine(offid + "TESTS");
                         if (d.Length == 4)
                         {
                             
@@ -66,17 +206,13 @@ namespace ConsoleApplication1
                         {
                             Console.WriteLine(c[i]);
                         }
-                       // if (west[1])
-                       // {
-                       //  }
-                        //var new_staff = new AMBULANCE_STAFF();
-                        //new_staff.FirstName = c[1];
-                       // new_staff.Surname =  c[2];
-                        //new_staff.OfficerID = Convert.ToInt32(c[4]);
-                        //new_staff.SkillLevel = c[6];
-                        //new_staff.Ambulance = null;
-                        //db.AMBULANCE_STAFFS.Add(new_staff);
-                       // db.SaveChanges();
+                        
+                        
+                        new_staff.Ambulance = null;
+                        db.AMBULANCE_STAFFS.Add(new_staff);
+                        db.SaveChanges();
+                        Console.WriteLine("The ambulance officer has been added");
+
                     }
                     //LIST FUNCTION ALL
                     if (line.Equals("list"))

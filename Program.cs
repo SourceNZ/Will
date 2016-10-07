b08cb25c49b87bbe40ac27d3e8e197d0264b50c5
SourceNZ <big.willy.99@hotmail.com>
2016-10-07 19:34:45 +1300

    added adding support

--- Program.cs
+++ Program.cs
@@ -11,32 +11,72 @@ namespace ConsoleApplication1
     {
         static void Main(string[] args)
         {
-            String line;
+            string line;
             Console.Write(">:");
             while (!((line = Console.ReadLine().ToLower()) == "exit"))
             {
 
                 using (AMBULANCES_STAFF_CONTEXT db = new AMBULANCES_STAFF_CONTEXT())
                 {
-                    //string[] wess = line.Split(new String[] {"as"}, StringSplitOptions.None);
-                    string[] west = line.Split(' ');
-                    if (west[0].Equals("add"))
+                    string[] a = line.ToLower().Split(new String[] {" as "}, StringSplitOptions.None);
+                    string[] b = line.ToLower().Split(new String[] { " id " }, StringSplitOptions.None);
+                    string[] c = line.Split(' ');
+                    if (c[0].Equals("add"))
                     {
+                        //c = line.Split(new String[] { "add " }, StringSplitOptions.None);
+                        //Console.WriteLine(c + "WILL");
+                        if(line.IndexOf(" id ") == -1)
+                        {
+                            Console.WriteLine("An officer must have a six digit ID number");
+                        }
+                        if (line.IndexOf(" as ") == -1)
+                        {
+                            Console.WriteLine("An officer musthave a skill level(basic, intermediate, or advanced)");
+                        }
+
+                        string v = line.Replace("add ", "");
+                        v = v.Replace(" id ", " ");
+                        v = v.Replace(" as ", " ");
+                        string[] d = v.Split(' ');
+                        Console.WriteLine(d.Length + "dfddsdsd");
+                        //if the user enters 4 different words either correctly.
+                        if (d.Length == 4)
+                        {
+                            
+                            Console.WriteLine("WILL" + d[0] + d[1] + d[2] + d[3] + "WILL");
+                        }
+                        
+                        
+                        // skill level .length should be 2  
+                        for (int i = 0; i < a.Length; i++)
+                        {
+                            Console.WriteLine(a[i] + " a {0}", a.Length);
+                        }
+                        Console.WriteLine("DONE");
+                        //id .length should be 2
+                        for (int i = 0; i < b.Length; i++)
+                        {
+                            //b[1]
+                            Console.WriteLine(b[i] + " b  : {0}", b.Length);
+                        }
+                        Console.WriteLine("DONE ID");
 
                         //need to reconvert the first letters of names to upper case and also check if user input is correct
-                        for (int i = 0; i < west.Length; i++)
+                        for (int i = 0; i < c.Length; i++)
                         {
-                            Console.WriteLine(west[i]);
+                            Console.WriteLine(c[i]);
                         }
-             
-                        var new_staff = new AMBULANCE_STAFF();
-                        new_staff.FirstName = west[1];
-                        new_staff.Surname =  west[2];
-                        new_staff.OfficerID = Convert.ToInt32(west[4]);
-                        new_staff.SkillLevel = west[6];
-                        new_staff.Ambulance = null;
-                        db.AMBULANCE_STAFFS.Add(new_staff);
-                        db.SaveChanges();
+                       // if (west[1])
+                       // {
+                       //  }
+                        //var new_staff = new AMBULANCE_STAFF();
+                        //new_staff.FirstName = c[1];
+                       // new_staff.Surname =  c[2];
+                        //new_staff.OfficerID = Convert.ToInt32(c[4]);
+                        //new_staff.SkillLevel = c[6];
+                        //new_staff.Ambulance = null;
+                        //db.AMBULANCE_STAFFS.Add(new_staff);
+                       // db.SaveChanges();
                     }
                     //LIST FUNCTION ALL
                     if (line.Equals("list"))

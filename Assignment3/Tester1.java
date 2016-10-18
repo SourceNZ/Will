11ec2b953793d60f1cbfbaf46140fc6f20d22cff
SourceNZ <big.willy.99@hotmail.com>
2016-10-18 15:13:09 +1300

    o

--- Assignment3/Tester1.java
+++ Assignment3/Tester1.java
@@ -1,6 +1,8 @@
 package Assignment3;
 
 import java.awt.Point;
+import java.awt.Rectangle;
+import java.awt.geom.Point2D;
 import java.io.BufferedReader;
 import java.io.File;
 import java.io.FileReader;
@@ -133,24 +135,14 @@ public class Tester1{
 //		At station: check if there is a new patient to pick up, if so,
 //		assign the closest unassigned patient to the ambulance and change the status to ‘Responding’. Otherwise, do nothing.		
 		
-		//for(Patient p : patientList2){
-		//	System.out.println(p.id + " " + p.status + " " +p.location+ "Initial Locaiton");
-		
-		//}
-		//System.out.println("Patients Done!");
-		Patient pe = null;
-		Ambulance be = null;
 		List<Point> ambulances = new ArrayList<Point>();
 
 		for(Ambulance a : ambulanceList2){
 			if(a.status.equals("At Station")){
 			 ambulances.add(new Point(Integer.parseInt(a.x_location),Integer.parseInt(a.y_location)));
-			 System.out.println(a.location + "Ambulances at stations");
 			}
 		}
-	
 		for(Ambulance a : ambulanceList2){
-			//System.out.println(a.id + " " +a.status + " " + a.location + "Initial Locaiton ");
 			if(a.status.equals("At Station")){
 				switch (a.location) {
 	            case "(50, 50)":  System.out.print ("HOSPITAL - ");
@@ -163,7 +155,6 @@ public class Tester1{
 	            		break;
 	            default : System.out.println ("NO STATION - " + a.location);
 	            		break;
-	     
 			}		
 				for(Patient p : patientList2){
 				Point pat = new Point(Integer.parseInt(p.x_location),Integer.parseInt(p.y_location));
@@ -177,24 +168,88 @@ public class Tester1{
 						a.status = "Responding";
 						a.patient = p.id;
 						p.ambulance = a.id;
-						System.out.println(a.id + " with status (" + a.status  + ") is picking up Patient " + p.id + " with status ("+ p.status+") ----" );
-						System.out.println();
-						break;
+						System.out.println(a.id + " with status (" + a.status  + ") is picking up Patient " + p.id + " with status ("+ p.status+") ..." );
 						
+						Responding(p, a);
+						break;
 					}
-
 				}
-				
 			}
-			
 		}
 		System.out.println("All Ambulances Assigned!");
-		for(Ambulance a : ambulanceList2){
-			System.out.println(a);
+	
+
+
+	
+}
+  private static void Responding(Patient p, Ambulance a) {
+		//‘Responding’: move the ambulance towards the assigned patient by four moves. 
+		//If the ambulance reaches the patient, change the status to ‘At scene’.
+		Point2D.Double p1 = new Point2D.Double(Integer.parseInt(a.x_location),Integer.parseInt(a.y_location));
+		Point2D.Double p2 = new Point2D.Double(Integer.parseInt(p.x_location),Integer.parseInt(p.y_location));
+		//p1 and p2 inits
+
+		// you don't use abs value and use the still point as the first one of the subtraction
+		double deltaX = p2.getX() - p1.getX();
+		double deltaY = p2.getY() - p1.getY();
+
+		// now you know how much far they are
+		double coeff = 0.25; //this coefficient can be tweaked to decide how much near the two points will be after the update.. 0.5 = 50% of the previous distance
+		
+		while(!p1.equals(p2)){
+		try {
+			p1.setLocation(p1.getX() + coeff*deltaX, p1.getY() + coeff*deltaY);
+			System.out.println(a.id + " with status (" + a.status  + ") Transporting Patient " +  p1 + " ..." );
+    	    Thread.sleep(1000);    
+       	} catch(InterruptedException ex) {
+    	    Thread.currentThread().interrupt();
+    	}
 		}
-		System.out.println("Ambulances Done!");
+		if(p1.equals(p2)){
+			
+			a.status = "At Scene";
+			//System.out.println(p1 + "At Scene");
+			System.out.println(a.id + " with status (" + a.status  + ") is at scene with Patient " + p.id + " with status ("+ p.status+") ..." );
+		}
+		//make it wait 4 seconds to do next step
+		//‘At scene’: if the ambulance has been at the scene for four seconds, change the status to ‘Transporting’. Otherwise, do nothing.
+		a.status = "Transporting";
+		p.status = "Transporting";
+		//‘Transporting’: move the ambulance towards the hospital by three moves. If the ambulance reaches the hospital, change the status to ‘At destination’.
+		System.out.println(a.id + " with status (" + a.status  + ") is going to hospital with Patient " + p.id + " with status ("+ p.status+") ..." );
+		Point2D.Double p3 = new Point2D.Double(Integer.parseInt(a.x_location),Integer.parseInt(a.y_location));
+		Point2D.Double hospital = new Point2D.Double(50,50);
 		
+		double deltaX1 = hospital.getX() - p3.getX();
+		double deltaY1 = hospital.getY() - p3.getY();
+
+		// now you know how much far they are
+		double coeff1 = 0.33333; //this coefficient can be tweaked to decide how much near the two points will be after the update.. 0.5 = 50% of the previous distance
+		p3.setLocation(p3.getX() + coeff1*deltaX1, p3.getY() + coeff1*deltaY1);
+		p3.setLocation(p3.getX() + coeff1*deltaX1, p3.getY() + coeff1*deltaY1);
+		p3.setLocation(p3.getX() + coeff1*deltaX1, p3.getY() + coeff1*deltaY1);
+		Rectangle rect = new Rectangle(45,45,10,10);//use your points co-ordinates 
+	    if (rect.contains(p3))
+	     {
+	    	a.status = "At destination";
+	    	p.status = "Completed";
+	    	System.out.println(a.id + " with status (" + a.status  + ") is At hospital with Patient " + p.id + " with status ("+ p.status+") ..." );
+	    	//if 2 seconds pass
+	    	try {
+	    	    Thread.sleep(2000);    
+	    	    a.status = "Returning";//1000 milliseconds is one second.
+	    	} catch(InterruptedException ex) {
+	    	    Thread.currentThread().interrupt();
+	    	}
+	    	
+	    	
+	    	
+	     }
+	    System.out.println();
+	    //‘At destination’: if the ambulance has been at the hospital for two seconds, change the status to ‘Returning’. Otherwise, do nothing.
+	
 		
+	
 		
 	}
 }
